1f0f7dc2733f84ea3a6356f6ad29b33594f6842b
liuzijie <fred.liu@ximalaya.com>
2017-12-19 17:24:53 +0800

    frist commit

---
+++ pye-client.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+import itchat
+from itchat.content import *
+import sys
+import os
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+@itchat.msg_register([TEXT])
+def simple_reply(msg):
+    if msg['Type'] != TEXT:
+        return
+    author = itchat.search_friends(nickName=r'sdserver')[0]
+    if msg['Content'][1:]!='':
+        print msg['Content'][1:]
+    commandstr = raw_input("[root@raspberry]$ ")
+    if commandstr=='exit':
+        os._exit(0)
+    author.send(commandstr)
+
+if __name__ == '__main__':
+    itchat.auto_login(enableCmdQR=0, hotReload=True)
+    author = itchat.search_friends(nickName=r'sdserver')[0]
+    author.send("hand")
+    itchat.run()
+    os._exit(0)
+    
+
---
+++ server/pye-server.py
@@ -0,0 +1,44 @@
+#coding=utf-8
+import itchat
+import commands
+from itchat.content import *
+import os
+import sys
+import re
+import time
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+pattern = re.compile(r'(vim|top|vi|ps|su|sh|tail).*')
+
+@itchat.msg_register([itchat.content.TEXT])
+def simple_reply(msg):
+    if msg['Type'] == TEXT:
+    	output=''
+    	try:
+	    	if pattern.match(msg['Content']):
+	    		output = msg['Content'] + ' is useless'
+	    	elif msg['Content'].startswith('cd '):
+	    		pwd=os.getcwd()+"/"  		
+	    		if(msg['Content'][3:].startswith('../')==True):
+	    			dirtemp = pwd + msg['Content'][3:]
+	    		else:
+	    			dirtemp = msg['Content'][3:]
+	    		os.chdir(dirtemp)
+	    		status=0
+	    		output=os.getcwd()
+	    	elif msg['Content']=='hand':
+	    		status=0
+	    		output='welcome '+msg['FromUserName']
+	    	else:
+	    		print str(commands.getstatusoutput('date')[1])
+	    		(status, output) = commands.getstatusoutput(msg['Content'])
+	    		print 'done'
+	    		if msg['Content'] == 'ls':
+	    			output = output.replace('\n', '\t')
+        except Exception,e:
+			itchat.send(u'error ,%s , %s' % (repr(e),output), msg['FromUserName'])
+			return ""
+        itchat.send(u'. %s' % (output), msg['FromUserName'])
+itchat.auto_login(enableCmdQR=0, hotReload=True)
+itchat.run()
